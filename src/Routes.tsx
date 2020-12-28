820ea75b92fb7125f7adf478659c912d71479e01
Matheus Ramyres <matheus.ramyres@academico.ifpb.edu.br>
2020-12-28 20:27:10 -0300

    Telas de denuncia

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,8 +3,10 @@ import { View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack'
 import Header from  './components/Header';
-import home from './pages/Home'
-import  DenunciaDetalhes from "./pages/DenunciaDetalhes"
+import home from './pages/Home';
+import selectPosition from './pages/CreateDenuncia/SelectMapPosition';
+import denunciaData from './pages/CreateDenuncia/DenunciaData';
+import  DenunciaDetalhes from "./pages/DenunciaDetalhes";
 
 // import { Container } from './styles';
 const {Navigator, Screen} = createStackNavigator();
@@ -13,16 +15,34 @@ const Routes: React.FC = () => {
     <NavigationContainer>
       
         <Navigator screenOptions={{headerShown:false}}>
-          <Screen 
-            name='home' component={home}  options={{
+
+        <Screen 
+            name='selectPosition' component={selectPosition}  options={{
             headerShown:true, 
+            header:()=><Header titulo='Marcar Denuncia'/> 
             }}
           />
+
+        <Screen 
+            name='denunciaData' component={denunciaData}  options={{
+            headerShown:true,
+            header:()=><Header titulo='Denunciar'/> 
+            }}
+          />
+          
+          <Screen 
+            name='home' component={home}  options={{
+            headerShown:true 
+            }}
+          />
+          
            <Screen 
             name='DenunciaDetalhes' component={DenunciaDetalhes}  options={{
             headerShown:true, 
             header:()=><Header titulo='Detalhes denuncia'/>}}
           />
+
+          
           
         </Navigator>
     </NavigationContainer>
